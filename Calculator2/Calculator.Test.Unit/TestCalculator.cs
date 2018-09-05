51c34aac87ee2d0518aeec53d2d6961499f32734
Crosty <Stanie_cheung@msn.com>
2018-09-05 18:20:53 +0200

    Update testCalculator

--- Calculator2/Calculator.Test.Unit/TestCalculator.cs
+++ Calculator2/Calculator.Test.Unit/TestCalculator.cs
@@ -25,6 +25,8 @@ namespace Calculator.Test.Unit
         [TestCase(-3, -2, -5)]
         [TestCase(-3, 2, -1)]
         [TestCase(3, -2, 1)]
+        [TestCase(3, 10, 13)]
+        [TestCase(5, 5, 10)]
         public void Add_AddPosAndNegNumbers_ResultIsCorrect(double a, double b, double result)
         {
             Assert.That(uut_.Add(a, b), Is.EqualTo(result));
