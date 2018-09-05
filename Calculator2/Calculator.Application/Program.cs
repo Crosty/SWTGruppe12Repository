ad664af91245a7aa92d1d6022256f4e0bb5cfd22
Crosty <Stanie_cheung@msn.com>
2018-09-05 16:57:44 +0200

    Added Stanies calculator solution

---
+++ Calculator2/Calculator.Application/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator2.Calculator test = new Calculator2.Calculator();
+
+            Console.WriteLine("Enter the first number: ");
+            string firstNumber = Console.ReadLine();
+            double a = double.Parse(firstNumber);
+
+            Console.WriteLine("Enter the second number: ");
+            string secondNumber = Console.ReadLine();
+            double b = double.Parse(secondNumber);
+
+            Console.WriteLine("Add:");
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {test.Add(a, b)}");
+
+            Console.WriteLine("\n\nSubtract:");
+            Console.Write($"{firstNumber} - {secondNumber} = {test.Subtract(a, b)}");
+
+            Console.WriteLine("\n\nMultiply:");
+            Console.Write($"{firstNumber} * {secondNumber} = {test.Multiply(a, b)}");
+
+            Console.WriteLine("\n\nPower:");
+            Console.Write($"{firstNumber} power to {secondNumber} = {test.Power(a, b)}");
+
+            Console.WriteLine("\n\nPress enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Calculator2/Calculator.Test.Unit/TestCalculator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Calculator2;
+
+namespace Calculator.Test.Unit
+{
+    [TestFixture]
+    [Author("Stanie Cheung")]
+    public class TestCalculator
+    {
+        private ICalculator uut_;
+
+        [SetUp]
+        public void Setup()
+        {
+            //Arrange
+            uut_ = new Calculator2.Calculator();
+        }
+
+        [TestCase(3, 2, 5)]
+        [TestCase(-3, -2, -5)]
+        [TestCase(-3, 2, -1)]
+        [TestCase(3, -2, 1)]
+        public void Add_AddPosAndNegNumbers_ResultIsCorrect(double a, double b, double result)
+        {
+            Assert.That(uut_.Add(a, b), Is.EqualTo(result));
+        }
+
+        [TestCase(3, 2, 1)]
+        [TestCase(-3, -2, -1)]
+        [TestCase(-3, 2, -5)]
+        [TestCase(3, -2, 5)]
+        public void Subtract_SubtractPosAndNegNumbers_ResultIsCorrect(double a, double b, double result)
+        {
+            Assert.That(uut_.Subtract(a, b), Is.EqualTo(result));
+        }
+
+
+        [TestCase(3, 2, 6)]
+        [TestCase(-3, -2, 6)]
+        [TestCase(-3, 2, -6)]
+        [TestCase(3, -2, -6)]
+        [TestCase(0, -2, 0)]
+        [TestCase(-2, 0, 0)]
+        [TestCase(0, 0, 0)]
+        public void Multiply_MultiplyNumbers_ResultIsCorrect(double a, double b, double result)
+        {
+            Assert.That(uut_.Multiply(a, b), Is.EqualTo(result));
+        }
+
+
+        [TestCase(2, 3, 8)]
+        [TestCase(2, -3, 0.125)]
+        [TestCase(-2, -3, -0.125)]
+        [TestCase(1, 10, 1)]
+        [TestCase(1, -10, 1)]
+        [TestCase(10, 0, 1)]
+        [TestCase(4, 0.5, 2.0)]
+        [TestCase(9, 0.5, 3.0)]
+        public void Power_RaiseNumbers_ResultIsCorrect(double x, double exp, double result)
+        {
+            Assert.That(uut_.Power(x, exp), Is.EqualTo(result));
+        }
+    }
+}
