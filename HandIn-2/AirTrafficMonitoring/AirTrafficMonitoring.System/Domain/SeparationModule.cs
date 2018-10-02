081d018302df07f518504004c6df6b152e450c05
Crosty <Stanie_cheung@msn.com>
2018-10-02 18:47:42 +0200

    Added ISeparationModule & SeparationModule

---
+++ HandIn-2/AirTrafficMonitoring/AirTrafficMonitoring.System/Domain/SeparationModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficMonitoring.System.Interfaces;
+
+namespace AirTrafficMonitoring.System.Domain
+{
+    public class SeparationModule : ISeparationModule
+    {
+        public event EventHandler<EventTracks> TracksSeparated;
+    }
+}
