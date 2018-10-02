a4ab380fbfd706d003fdb3fa3214f6d17e16f63f
Crosty <Stanie_cheung@msn.com>
2018-10-02 20:21:34 +0200

    Created methods in SeparationModule

--- HandIn-2/AirTrafficMonitoring/AirTrafficMonitoring.System/Domain/SeparationModule.cs
+++ HandIn-2/AirTrafficMonitoring/AirTrafficMonitoring.System/Domain/SeparationModule.cs
@@ -9,8 +9,33 @@ namespace AirTrafficMonitoring.System.Domain
 {
     public class SeparationModule : ISeparationModule
     {
+        private ILog _log;
+        private List<ITrack> _oldSeparations;
         public event EventHandler<EventTracks> TracksSeparated;
 
+        public SeparationModule(IUpdateModule updateModule, ILog log)
+        {
+            _log = log;
+            _oldSeparations = new List<ITrack>();
+            updateModule.TracksUpdated += SeparationTracks;
+        }
 
+        private void SeparationTracks(object sender, EventTracks e)
+        {
+            var updatedSeparations = new List<ITrack>();
+
+            foreach (var separationTrack in e.Data)
+            {
+                //Do something
+            }
+
+            _oldSeparations = updatedSeparations;
+
+        }
+
+        protected virtual void UpdatedSeparationsEvent(EventTracks e)
+        {
+            TracksSeparated?.Invoke(this, e);
+        }
     }
 }
