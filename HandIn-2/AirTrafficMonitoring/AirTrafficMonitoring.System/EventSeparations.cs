3043e6e041b04e50b5be8a4ca48e51454977aa6e
Crosty <Stanie_cheung@msn.com>
2018-10-03 11:37:29 +0200

    Added EventSeparations

---
+++ HandIn-2/AirTrafficMonitoring/AirTrafficMonitoring.System/EventSeparations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficMonitoring.System.Interfaces;
+
+namespace AirTrafficMonitoring.System
+{
+    public class EventSeparations : EventArgs
+    {
+        public List<ISeparation> sepData;
+
+        public EventSeparations(List<ISeparation> sepdata)
+        {
+            sepData = sepdata;
+        }
+    }
+}
