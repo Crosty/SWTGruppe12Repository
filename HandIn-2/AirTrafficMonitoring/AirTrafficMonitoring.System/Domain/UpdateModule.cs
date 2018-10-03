d847683dd3fbe8363cea6ac087ec9bd6eaaad099
Crosty <Stanie_cheung@msn.com>
2018-10-03 12:20:05 +0200

    Updated UpdateModule

--- HandIn-2/AirTrafficMonitoring/AirTrafficMonitoring.System/Domain/UpdateModule.cs
+++ HandIn-2/AirTrafficMonitoring/AirTrafficMonitoring.System/Domain/UpdateModule.cs
@@ -49,9 +49,14 @@ namespace AirTrafficMonitoring.System.Domain
         //Calculates velocity of the tracks
         private double CalculateVelocity(Point oldPoint, Point newPoint, DateTime oldTime, DateTime newTime)
         {
+            //TimeSpan represents a time interval - time interval between the old and new.
             TimeSpan time = newTime - oldTime;
+            //Gets the value of the current TimeSpan - represents the total number of seconds.
             var timeDiff = time.TotalSeconds;
+            //Square root of two Power values, in this case the coordination of the old and new.
+            //This shows the distance it moved during the TimeSpan
             var distance = Math.Sqrt(Math.Pow((newPoint.X - oldPoint.X), 2) + Math.Pow((newPoint.Y - oldPoint.Y), 2));
+            //Calculates the speed
             var velocity = distance / timeDiff;
 
             return (int)velocity;
@@ -60,6 +65,7 @@ namespace AirTrafficMonitoring.System.Domain
         //Calculates course of the tracks
         private double CalculateCourse(Point oldPoint, Point newPoint)
         {
+            //Math.Abs: Absolute value, in case it goes below 0.
             double xcoord = Math.Abs(newPoint.X - oldPoint.X);
             double ycoord = Math.Abs(newPoint.Y - oldPoint.Y);
 
@@ -71,11 +77,11 @@ namespace AirTrafficMonitoring.System.Domain
             }
             else
             {
-
+                //Returns an angle measured in radian
                 double radian = Math.Atan2(ycoord, xcoord);
                 direction = radian / Math.PI * 180;
 
-                direction = 90 - direction;
+                direction -= 90;
                 if (direction < 0)
                 {
                     direction += 360;
