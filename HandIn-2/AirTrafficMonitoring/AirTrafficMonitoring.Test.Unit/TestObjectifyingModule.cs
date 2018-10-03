693b56244e81f799541729fe0036433e75dc9b43
Crosty <Stanie_cheung@msn.com>
2018-10-03 14:49:42 +0200

    Merging

--- HandIn-2/AirTrafficMonitoring/AirTrafficMonitoring.Test.Unit/TestObjectifyingModule.cs
+++ HandIn-2/AirTrafficMonitoring/AirTrafficMonitoring.Test.Unit/TestObjectifyingModule.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AirTrafficMonitoring.System.Interfaces;
+using AirTrafficMonitoring.System.Domain;
+using TransponderReceiver;
+using NUnit.Framework;
 
 namespace AirTrafficMonitoring.Test.Unit
 {
+    [TestFixture]
+    [Author("SWTGruppe12")]
     public class TestObjectifyingModule
     {
+        private ITransponderReceiver _tr;
+
+        private IObjectifyingModule _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
     }
 }
