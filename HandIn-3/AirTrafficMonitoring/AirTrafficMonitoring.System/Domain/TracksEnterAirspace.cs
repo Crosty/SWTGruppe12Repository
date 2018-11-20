e1d658539e4a28f633d6934eaa48425e95957c6b
Crosty <Stanie_cheung@msn.com>
2018-11-20 14:35:01 +0100

    First sketch TracksEnterAirspace

--- HandIn-3/AirTrafficMonitoring/AirTrafficMonitoring.System/Domain/TracksEnterAirspace.cs
+++ HandIn-3/AirTrafficMonitoring/AirTrafficMonitoring.System/Domain/TracksEnterAirspace.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using AirTrafficMonitoring.System.Interfaces;
 
 namespace AirTrafficMonitoring.System.Domain
@@ -10,28 +11,54 @@ namespace AirTrafficMonitoring.System.Domain
     public class TracksEnterAirspace : ITracksEnterAirspace
     {
         private IAirspace _airspace;
+        private List<ITrack> _currentTracks;
         public event EventHandler<EventTracks> TracksEnteredAirspace;
 
         public TracksEnterAirspace(IAirspace airspace, IUpdateModule updateModule)
         {
             _airspace = airspace;
+            _currentTracks = new List<ITrack>();
             updateModule.TracksUpdated += TrackEnterAirspace;
         }
 
         private void TrackEnterAirspace(object sender, EventTracks e)
         {
             var enteredTracks = new List<ITrack>();
-            foreach (var checkBorder in e.Data)
+            foreach (var track in e.Data)
             {
                 //Check if within airspace, if within airspace printout "YALLAH I HJEMME"
                 //Else, printout "YALLAH SKRID MED JER".
+
+                if (CheckIfTrackEntersAirspace(track.Timestamp, track.Position))
+                {
+                    enteredTracks.Add(track);
+                }   
             }
             // Update the list.
+            TracksEnteredAirspaceEvent(new EventTracks(enteredTracks));
         }
 
         protected virtual void TracksEnteredAirspaceEvent(EventTracks e)
         {
+            TracksEnteredAirspace?.Invoke(this, e);
+        }
 
+        //Checks if the a track enters airspace
+        private bool CheckIfTrackEntersAirspace(DateTime timestamp, Point point)
+        {
+            var southWestCorner = new Point(10000, 10000, 500);
+            var northEastCorner = new Point(90000, 90000, 20000);
+            var timer = new Timer();
+
+            if (point.X > southWestCorner.X && point.Y > southWestCorner.Y && point.X < northEastCorner.X &&
+                point.Y < northEastCorner.Y)
+            {
+                Console.WriteLine("*Track Entered Airspace*" + " - Tag: " + _currentTracks.FirstOrDefault()?.Tag + ", Time: " + timestamp);
+                timer.Enabled = true;
+                timer.Interval = 5000;
+                return true;
+            }
+            return false;
         }
     }
 }
