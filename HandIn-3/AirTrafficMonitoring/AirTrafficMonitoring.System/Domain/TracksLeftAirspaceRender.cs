f8ca9ef12353c71cdd59f0f84cb7619a0bc49b9e
Crosty <Stanie_cheung@msn.com>
2018-11-21 14:02:39 +0100

    Merging

---
+++ HandIn-3/AirTrafficMonitoring/AirTrafficMonitoring.System/Domain/TracksLeftAirspaceRender.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using AirTrafficMonitoring.System.Interfaces;
+
+namespace AirTrafficMonitoring.System.Domain
+{
+   public class TracksLeftAirspaceRender : ITracksLeftAirspaceRender
+    {
+        private readonly IDisplay _display;
+        
+        public TracksLeftAirspaceRender(ITrackLeftAirspace TracksleftAirspace, IDisplay display)
+        {
+            _display = display;
+            TracksleftAirspace.TracksleftAirspace += RenderLeftTracks;
+
+        }
+
+        private void RenderLeftTracks(object sender, EventTracks e)
+        {
+            _display.Write("*TRACKS LEFT AIRSPACE*/n");
+            foreach (var track in e.Data)
+            {
+                var timer = new Timer();
+                var str = "Tag: " + track.Tag + ", Time: " + track.Timestamp.ToString("G");
+
+                timer.Enabled = true;
+                timer.Interval = 5000;
+                _display.Write(str);
+            }
+        }
+
+    }
+}
