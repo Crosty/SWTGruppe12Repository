3f788d1aca8f907b65d25fa6b755f17f2efbbcfb
Crosty <Stanie_cheung@msn.com>
2018-11-21 20:46:54 +0100

    Fixing the HEAD issue

--- HandIn-3/AirTrafficMonitoring/AirTrafficMonitoring.Test.Unit/TestUpdateModule.cs
+++ HandIn-3/AirTrafficMonitoring/AirTrafficMonitoring.Test.Unit/TestUpdateModule.cs
@@ -21,18 +21,4 @@ namespace AirTrafficMonitoring.Test.Unit
     //    //Required
     //    private Point _insideAirspacePoint;
     //}
-=======
-    [TestFixture]
-    [Author("SWTGruppe12")]
-    public class TestUpdateModule
-    {
-        ////Demand
-        //private IFilterModule _filterModule;
-        //private List<ITrack> _tracks;
-        ////uut
-        //private IUpdateModule _uut;
-        ////Required
-        //private Point _insideAirspacePoint;
-    }
->>>>>>> 77ba23b64c1f81226555489e372c07bad22e1b03
 }
