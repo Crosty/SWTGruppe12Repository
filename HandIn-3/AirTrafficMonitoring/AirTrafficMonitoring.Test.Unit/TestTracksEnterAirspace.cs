17030e30523fc1f1cfd66e6d77da20103338b02a
Mehmetali_Duman <20161166@uni.au.dk>
2018-11-22 17:46:43 +0100

    Updated

--- HandIn-3/AirTrafficMonitoring/AirTrafficMonitoring.Test.Unit/TestTracksEnterAirspace.cs
+++ HandIn-3/AirTrafficMonitoring/AirTrafficMonitoring.Test.Unit/TestTracksEnterAirspace.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using AirTrafficMonitoring.System.Interfaces;
+using AirTrafficMonitoring.System.Domain;
+using TransponderReceiver;
+using NUnit.Framework;
+using NSubstitute;
 namespace AirTrafficMonitoring.Test.Unit
 {
+
+    [TestFixture]
+    [Author("SWTGruppe12")]
     class TestTracksEnterAirspace
     {
+        private ITracksEnterAirspace _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+
+
+        }
+
     }
 }
