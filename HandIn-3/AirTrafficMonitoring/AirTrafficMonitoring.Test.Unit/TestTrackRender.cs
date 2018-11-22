3085232b4c38d0bfc9b57f4e98839c3fa217a6f4
Crosty <Stanie_cheung@msn.com>
2018-11-22 17:33:02 +0100

    Merge attempt

---
+++ HandIn-3/AirTrafficMonitoring/AirTrafficMonitoring.Test.Unit/TestTrackRender.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirTrafficMonitoring.System;
+using AirTrafficMonitoring.System.Domain;
+using AirTrafficMonitoring.System.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace AirTrafficMonitoring.Test.Unit
+{
+    [TestFixture]
+    [Author("SWTGruppe12")]
+
+    public class TestTrackRender
+    {
+
+        private IDisplay _display;
+        private IUpdateModule _updateModule;
+
+        private TrackRender _uut;
+    
+
+        [SetUp]
+
+        public void Setup()
+        {
+
+            _updateModule = Substitute.For<IUpdateModule>();
+            _display = Substitute.For<IDisplay>();
+
+        
+
+
+
+
+
+        }
+
+
+
+    }
+
+}
