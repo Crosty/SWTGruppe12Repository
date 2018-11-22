a5167f24c5462ce5901678a5f064a1ef902e5110
Mehmetali_Duman <20161166@uni.au.dk>
2018-11-22 18:53:25 +0100

    Updated

--- HandIn-3/AirTrafficMonitoring/AirTrafficMonitoring.Test.Unit/TestTracksLeftAirspaceRender.cs
+++ HandIn-3/AirTrafficMonitoring/AirTrafficMonitoring.Test.Unit/TestTracksLeftAirspaceRender.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using AirTrafficMonitoring.System;
+using AirTrafficMonitoring.System.Domain;
+using AirTrafficMonitoring.System.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
 namespace AirTrafficMonitoring.Test.Unit
 {
+    [TestFixture]
+    [Author("SWTGruppe12")]
     class TestTracksLeftAirspaceRender
     {
+        private ITrackLeftAirspace _trackLeftAirspace;
+        private IDisplay _display;
+
+        private ITracksLeftAirspaceRender _uut;
+        //Required
+        private Point _trackOne;
+        private Point _trackTwo;
     }
 }
