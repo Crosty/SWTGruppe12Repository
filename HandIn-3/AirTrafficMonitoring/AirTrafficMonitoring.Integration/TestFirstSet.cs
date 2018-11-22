62ef19037061d3d0ce4af9c9815cdd206f9de407
Crosty <Stanie_cheung@msn.com>
2018-11-22 20:30:01 +0100

    Added & Tested TestFirstSet

--- HandIn-3/AirTrafficMonitoring/AirTrafficMonitoring.Integration/TestFirstSet.cs
+++ HandIn-3/AirTrafficMonitoring/AirTrafficMonitoring.Integration/TestFirstSet.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AirTrafficMonitoring.System.Domain;
 using AirTrafficMonitoring.System.Interfaces;
 using NSubstitute;
 using NUnit.Framework;
@@ -24,13 +25,45 @@ namespace AirTrafficMonitoring.Integration
         //Required
         private IObjectifyingModule _objectifyingModule;
         private IAirspace _airspace;
-        private List<ITrack> _createdTrackList;
         private List<ITrack> _filteredTrackList;
+
         [SetUp]
         public void SetUp()
         {
             //Arrange
             _tr = Substitute.For<ITransponderReceiver>();
+
+            _objectifyingModule = new ObjectifyingModule(_tr);
+            _airspace = new Airspace(); 
+
+            _iut = new FilterModule(_objectifyingModule, _airspace);
+
+            _iut.TracksFiltered += (sender, args) =>
+            {
+                _filteredTrackList = args.Data;
+            };
+        }
+
+        [TestCase("Tag23;10345;43562;4350;20160410235943156", "Tag24;24503;70594;2103;20180410135959931")]
+        public void FirstSet_CreateTracksWithObjectifyingModule_FilterTracks(string trackOne, string trackTwo)
+        {
+            var split1 = trackOne.Split(';')[0];
+            var split2 = trackTwo.Split(';')[0];
+
+            var data = new List<string>();
+            var args = new RawTransponderDataEventArgs(data);
+
+            var track1 = trackOne;
+            var track2 = trackTwo;
+
+            data.Add(track1);
+            data.Add(track2);
+
+            _tr.TransponderDataReady += Raise.EventWith(args);
+
+            Assert.That(_filteredTrackList.Count == 2);
+            Assert.That(_filteredTrackList[0].Tag, Is.EqualTo(split1));
+            Assert.That(_filteredTrackList[1].Tag, Is.EqualTo(split2));
         }
     }
 }
